471d65abe499547cc3b67e17f5c70c4ad20b6e84
bransonf <32143148+bransonf@users.noreply.github.com>
2020-01-03 18:41:55 -0600

    full geocodio

--- source/geocodio/geocodio.R
+++ source/geocodio/geocodio.R
@@ -1,24 +1,46 @@
-# [ ] Geocodio (Batch 10K) SEE Docs: https://www.geocod.io/docs/#geocoding
-geocodio <- function(json, key){
+# Geocodio 
+library(httr);library(magrittr);library(dplyr);library(jsonlite)
+
+# Batch 10,000
+# Docs: https://www.geocod.io/docs/#geocoding
+
+geocodio <- function(addresses, key){
   url = 'https://api.geocod.io/v1.4/geocode'
   POST(url,
        query = list(
          api_key = key
        ),
-       body = json # if valid json, else use list and encode = 'json'
+       body = jsonlite::toJSON(addresses),
+       content_type_json()
   ) %>%
-    content()
-  # Allows for Address Components
+  content() -> P
   
-  # Arrary
-  # [
-  #   "<Address>"
-  #   "<Address>"
-  # ]
+  # parse the response
+  parsed <- lapply(P$results, function(x){
+    data.frame(stringsAsFactors = FALSE,
+               address = x[["query"]],
+               lat = x[["response"]][["results"]][[1]][["location"]]$lat,
+               lng = x[["response"]][["results"]][[1]][["location"]]$lng)
+  })
   
-  # Or object
-  # {
-  #   "1" : <Address>"
-  #   "2" : <Address>"
-  # }
+  parsed <- dplyr::bind_rows(parsed)
+  
+  return(parsed)
+  
 }
+
+# Load Vector of Addresses
+data <- read.csv('data/STL_CRIME_Homicides.csv', stringsAsFactors = FALSE)['address_norm']
+data <- simplify2array(data)
+data <- paste0(data, ' St. Louis, MO') # Add St. Louis Key (Some Blanks though...)
+addresses <- data
+
+# Load Key
+gkey <- yaml::read_yaml('creds.yml')$geocodio
+
+# Full Test Run
+gtime <- system.time({
+  gfull <- geocodio(addresses, gkey) 
+})
+
+save(gtime, gfull, file = 'results/geocodio.rda')
