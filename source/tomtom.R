d4c125c1f108fe3b8941936de145f9d0acd48d73
bransonf <32143148+bransonf@users.noreply.github.com>
2020-01-04 22:16:18 -0600

    full tomtom

--- source/tomtom.R
+++ source/tomtom.R
@@ -6,45 +6,49 @@ library(httr);library(magrittr);library(dplyr)
 
 # Asynchrnous Unstructured Implementation
 tomtom <- function(addresses, key){
-  # Standardize Data (Build own json object)
-  schema <- '{"batchItems":['
-  for (i in seq_along(addresses)){
-    schema <- paste0(
-      schema, '{"query":"/geocode/', addresses[i], '.json"},'
-    )
+  # TomTom's Async Implementation is Bad, so iterate batches of 100
+  # * It's bad because it uses long polling that times out in 120 seconds regardless
+  jobs <- split(addresses, (seq(length(addresses))-1) %/% 100)
+  returns <- vector('list', length(jobs))
+  
+  for (i in seq_along(jobs)){
+    # Standardize Data (Build own json object)
+    schema <- '{"batchItems":['
+    for (j in seq_along(jobs[[i]])){
+      schema <- paste0(
+        schema, '{"query":"/geocode/', jobs[[i]][j], '.json"},'
+      )
+    }
+      
+      # Remove last comma with closing brackets
+    schema <- gsub(',$', ']}', schema)
+    
+    # Submit Job
+    url = 'https://api.tomtom.com/search/2/batch.json'
+    
+    POST(url,
+         query = list(
+           key = key,
+           redirectMode = 'auto' # 303, Long Poll 120 Seconds
+         ),
+         body = schema,
+         content_type_json()
+    ) %>%
+    content() -> P
+    
+    # Pre-Parse
+    returns[[i]] <- lapply(P$batchItems, function(x){
+      data.frame(stringsAsFactors = FALSE,
+                 lower = x$response$summary$query, # Replaced with original capitalization in parsing
+                 lat = x$response$results[[1]]$position$lat,
+                 lon = x$response$results[[1]]$position$lon)
+    })
+    
+    returns[[i]] <- dplyr::bind_rows(returns[[i]])
   }
-    
-    # Remove last comma with closing brackets
-  schema <- gsub(',$', ']}', schema)
-  
-  # Submit Job
-  url = 'https://api.tomtom.com/search/2/batch.json'
-  
-  POST(url,
-       query = list(
-         key = key,
-         redirectMode = 'manual'
-       ),
-       body = schema,
-       content_type_json()
-  ) %>%
-  headers() -> P
-    
-  dl_url <- paste0('https://api.tomtom.com', P$location)
-  
-  # Download (May need to increase timeout)
-  GET(dl_url) %>%
-  content() -> response
   
   # Parse
-  parsed <- lapply(response$batchItems, function(x){
-    data.frame(stringsAsFactors = FALSE,
-               lower = x$response$summary$query, # Replaced with original capitalization in parsing
-               lat = x$response$results[[1]]$position$lat,
-               lon = x$response$results[[1]]$position$lon)
-  })
-  
-  parsed <- dplyr::bind_rows(parsed)
+  parsed <- dplyr::bind_rows(returns)
   parsed <- dplyr::mutate(parsed, row = row_number())
   
     # Match lower case query to original capitalization
@@ -55,7 +59,7 @@ tomtom <- function(addresses, key){
   
   parsed <- dplyr::left_join(parsed, lower_key, by = c('lower', 'row'))
   
-  # Remove Lower Column
+  # Remove Lower Columns
   parsed <- parsed[,c('address', 'lat', 'lon')]
   
   return(parsed)
